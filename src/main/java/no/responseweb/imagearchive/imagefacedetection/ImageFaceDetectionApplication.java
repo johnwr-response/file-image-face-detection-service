203331b1d823513ea74751cc241e5cef07c28d03
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-07-01 17:41:39 +0200

    init

---
+++ src/main/java/no/responseweb/imagearchive/imagefacedetection/ImageFaceDetectionApplication.java
@@ -0,0 +1,17 @@
+package no.responseweb.imagearchive.imagefacedetection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ImageFaceDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageFaceDetectionApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/no/responseweb/imagearchive/imagefacedetection/ImageFaceDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package no.responseweb.imagearchive.imagefacedetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageFaceDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
